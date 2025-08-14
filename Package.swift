98fe8bc63cbb0aaedfd656cc71ae0debacb7ea38
knt.t <153593861+kntt32@users.noreply.github.com>
2025-08-14 12:53:59 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,10 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "ZipUtl",
-    platforms: [
-        .macOS(.v15),
-        .iOS(.v18)
-    ],
     products: [
         .library(
             name: "ZipUtl",
